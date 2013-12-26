12c6e4c8957b3314438d5e8fde74674ca1031d8e
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2013-12-26 20:59:12 +1100

    Remove redundant IIFE, clean up comments

--- app/templates/src/name.js
+++ app/templates/src/name.js
@@ -1,41 +1,39 @@
-(function(bespoke) {
+bespoke.plugins.<%= pluginNameCamelized %> = function(deck) {
 
-  bespoke.plugins.<%= pluginNameCamelized %> = function(deck) {
+  /*
+    Interact with the deck
+    https://github.com/markdalgleish/bespoke.js#deck-instances
+  */
+  deck.next();
+  deck.prev();
+  deck.slide(0);
 
-    /*
-      Interact with the deck
-      https://github.com/markdalgleish/bespoke.js#deck-instances
-    */
-    deck.next();
-    deck.prev();
-    deck.slide(0);
+  /*
+     Slide events
+     https://github.com/markdalgleish/bespoke.js#events
+  */
+  deck.on('activate', function(e) {
+    console.log('Slide #' + e.index + ' was activated!', e.slide);
+  });
 
-    /*
-       Handle events
-       https://github.com/markdalgleish/bespoke.js#events
-    */
-    deck.on('activate', function(e) {
-      console.log('Slide #' + e.index + ' was activated!', e.slide);
-    });
+  deck.on('deactivate', function(e) {
+    console.log('Slide #' + e.index + ' was deactivated!', e.slide);
+  });
 
-    deck.on('deactivate', function(e) {
-      console.log('Slide #' + e.index + ' was deactivated!', e.slide);
-    });
+  /*
+    Deck interaction events, return false to cancel
+    https://github.com/markdalgleish/bespoke.js#deck-interaction-events
+  */
 
-    deck.on('next', function(e) {
-      console.log('The next slide is #' + (e.index + 1), deck.slides[e.index + 1]);
-      // return false to cancel user interaction
-    });
+  deck.on('next', function(e) {
+    console.log('The next slide is #' + (e.index + 1), deck.slides[e.index + 1]);
+  });
 
-    deck.on('prev', function(e) {
-      console.log('The previous slide is #' + (e.index - 1), deck.slides[e.index - 1]);
-      // return false to cancel user interaction
-    });
+  deck.on('prev', function(e) {
+    console.log('The previous slide is #' + (e.index - 1), deck.slides[e.index - 1]);
+  });
 
-    deck.on('slide', function(e) {
-      console.log('The requested slide is #' + e.index, e.slide);
-      // return false to cancel user interaction
-    });
-  };
-
-}(bespoke));
+  deck.on('slide', function(e) {
+    console.log('The requested slide is #' + e.index, e.slide);
+  });
+};
